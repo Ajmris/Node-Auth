cfbee42caec428004f8da077ad85e8ae4eac660f
ajmris <szymektarasiewicz@onet.pl>
2022-11-28 00:01:44 +0100

    Validation

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://';
+const dbURI = 'mongodb+srv:/';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
