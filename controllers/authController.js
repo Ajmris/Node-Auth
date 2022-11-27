5947a03df8e79b2e44a02cb0143cf2a6450b87b8
ajmris <szymektarasiewicz@onet.pl>
2022-11-27 23:56:40 +0100

    Testing Routes & Handling POST Requests

---
+++ controllers/authController.js
@@ -0,0 +1,25 @@
+const { rest } = require('lodash');
+const User=require('../models/User');
+module.exports.signup_get=(req, res)=>{
+    res.render('signup');
+}
+module.exports.login_get=(req, res)=>{
+    res.render('login');
+}
+module.exports.signup_post= (req, res)=>{
+    const {email,password}=req.body;
+    console.log(email, password);
+    res.send('Nowy zapis.');
+    /*try{
+        const user=await User.create({email, password});
+        res.status(201).json(user);
+    }catch(err){
+        console.log(err);
+        res.status(400).send('error, user not created.');
+    }*/
+}
+module.exports.login_post=(req, res)=>{
+    const {email, password}=req.body;
+    console.log(email, password);
+    res.send('user login');
+}
