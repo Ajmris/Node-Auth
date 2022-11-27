8c6a4723091d20c67bc9c0fc7069b2b98d9941ca
ajmris <szymektarasiewicz@onet.pl>
2022-11-28 00:08:25 +0100

    Login and Signup backend

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-//const dbURI = 'mongodb+srv://';
-const dbURI = '';
+//const dbURI = 'mongodb+srv://
+const dbURI = 'mongodb+srv://';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
@@ -23,16 +23,3 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
-
-//cookies
-app.get('/set-cookies', (req,res)=>{
-  //res.setHeader('Set-Cookie', 'newUser=true');
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, {maxAge: 1000*60*60*24, httpOnly:true});
-  res.send('You got the Cookies!');
-});
-app.get('/read-cookies', (req,res)=>{
-  const cookies=req.cookies;
-  console.log(cookies.newUser);
-  res.json(cookies);
-});
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,18 @@
 const User=require('../models/User');
+const jwt=require('jsonwebtoken')
 
 const handleErrors=(err)=>{
     console.log(err.message, err.code);
     let errors={email: '', password: ''};
 
+    // incorrect email
+    if(err.message==='incorrect email'){
+        errors.email='that email is not registered';
+    }
+    // incorrect password
+    if(err.message==='incorrect email'){
+        errors.password='that password is incorect';
+    }
     // duplicate error code
     if(err.code===11000){
         errors.email='That email is already registered.';
@@ -17,6 +26,12 @@ const handleErrors=(err)=>{
     }
     return errors;
 }
+const maxAge= 3*24*60*60;
+const createToken=(id)=>{
+    return jwt.sign({id}, 'net ninja secret', {
+        expiresIn: maxAge
+    });
+}
 module.exports.signup_get=(req, res)=>{
     res.render('signup');
 }
@@ -27,7 +42,9 @@ module.exports.signup_post=async (req, res)=>{
     const {email,password}=req.body;
     try{
         const user=await User.create({email, password});
-        res.status(201).json(user);
+        const token=createToken(user._id);
+        res.cookie('jwt', token, {httpOnly:true,maxAge:maxAge*1000});
+        res.status(201).json({user: user._id});
     }catch(err){
         const errors=handleErrors(err);
         res.status(400).json({errors});
@@ -35,6 +52,13 @@ module.exports.signup_post=async (req, res)=>{
 }
 module.exports.login_post=async (req, res)=>{
     const {email, password}=req.body;
-    console.log(email, password);
-    res.send('user login');
+    try{
+        const user=await User.login(email, password);
+        const token=createToken(user._id);
+        res.cookie('jwt', token, {httpOnly:true,maxAge:maxAge*1000});
+        res.status(200).json({user: user._id});
+    }catch(err){
+        const errors=handleErrors(err);
+        res.status(400).json({errors});
+    }
 }
