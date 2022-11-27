a8024857ede0f31becc07a111041ceda42c75f37
ajmris <szymektarasiewicz@onet.pl>
2022-11-28 00:13:06 +0100

    Show login

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes= require('./routes/authRoutes');
 const cookieParser=require('cookie-parser');
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const app = express();
 
 // middleware
@@ -14,13 +14,14 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-//const dbURI = 'mongodb+srv';
-const dbURI = 'mongodb+srv://';
+//const dbURI = 'mongodb+srv://';
+const dbURI = 'mongodb+srv://;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt=require('jsonwebtoken');
+const User = require('../models/User');
 const requireAuth=(req,res,next)=>{
     const token=req.cookies.jwt;
     // sprawdź czy token istnieje i jest zweryfiokowany
@@ -16,4 +17,26 @@ const requireAuth=(req,res,next)=>{
         res.redirect('/login');
     }
 }
-module.exports={requireAuth};
+
+//check current user
+const checkUser=(req, res,next)=>{
+    const token=req.cookies.jwt;
+    if(token){
+        jwt.verify(token, 'net ninja secret', async (err, decodedToken)=>{
+            if(err){
+                console.log(err.message);
+                res.locals.user=null;
+                next();
+            }else{
+                console.log(decodedToken);
+                let user=await User.findById(decodedToken.id);
+                res.locals.user=user;
+                next();
+            }
+        })
+    }else{
+        res.locals.user=null;
+        next();
+    }
+}
+module.exports={requireAuth, checkUser};
